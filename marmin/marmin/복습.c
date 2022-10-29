52adf2cf93a1285cdd386828e02556a420b3b1f0
marmin <112798662+marmin-dev@users.noreply.github.com>
2022-10-29 21:56:20 +0900

    12

---
+++ marmin/marmin/복습.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+	int floor;
+	printf("몇층으로 쌓을겁니까?");
+	scanf_s("%d", &floor);
+	for (int i = 0; i < floor; i++)
+	{
+		for (int j = i; j <= floor - 1; j++)
+		{
+			printf(" ");
+			{
+				for (int k = 0; k <= i * 2; k++)
+				{
+					printf("*");
+				}
+				printf("\n");
+			}
+		}
+	}
+}
